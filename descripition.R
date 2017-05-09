69938fafc02b54af73ea304c14c14e9535199017
Erik Räni <erik.rani@mail.ee>
2017-05-09 21:12:01 +0300

    #2 Plot of Distributions of Articles about Companies

--- descripition.R
+++ descripition.R
@@ -1,4 +1,5 @@
 library(readxl)
+library(ggplot2)
 
 fetch_descr <- function(file, range) {
   raw <- as.data.frame(read_excel(file, range = range, trim_ws = TRUE))
@@ -9,3 +10,17 @@ fetch_descr <- function(file, range) {
 }
 
 descriptive_stats <- fetch_descr("All data file.xlsx", "Sheet1!A3:K48")
+descr_no_articles <- 
+  descriptive_stats[order(descriptive_stats$`No. Articles`),c(1,2)]
+
+articles_distribution <- 
+  ggplot(descr_no_articles, 
+         aes(reorder(Ticker, -`No. Articles`),`No. Articles`, fill=`No. Articles`))+
+  geom_bar(stat="identity")+ 
+  theme(axis.text.x=element_text(angle=90))+
+  xlab("Ticket") +
+  ylab("No. Articles") +
+  ggtitle("Distribution of Articles about Companies")
+
+# Plot Distributions of Articles about Companies
+plot(articles_distribution)
