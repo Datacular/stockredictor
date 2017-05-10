2f714fe43c3ce087fa06f7862a1584907da40372
Erik Räni <erik.rani@mail.ee>
2017-05-11 01:04:36 +0300

    #3 Articles raw text is replaced by words by tokenizer.

--- textmining.R
+++ textmining.R
@@ -1,5 +1,18 @@
 library(readxl)
+library(tidytext)
+library(tm)
+library(dplyr)
 
 articles <- as.data.frame(read_excel("All data file.xlsx",
                                      range = "Content!A1:O25987",
                                      trim_ws = TRUE))
+
+tokenizer <- function(df){
+  for (i in 1:nrow(df)) {
+    df[i,"Content"] <- paste((tidy(VCorpus(VectorSource(df[i,"Content"]))) %>% 
+                            unnest_tokens(word, text))$word, collapse = ';')
+  }
+  return(df)
+}
+
+articles <- tokenizer(articles)
